f591446901c471891c45a3f91e72c8e45053b0be
ikun <0215.miyatto@gmail.com>
2023-05-23 04:23:15 +0900

    create item機能の実装

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,11 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { ItemsService } from './items.service';
+import { ItemStatus } from './item.status.enum';
+import { Item } from './item.model';
 
 @Controller('items')
 export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
+
   @Get()
   findAll() {
     return this.itemsService.findAll();
+  }
+
+  @Post()
+  create(
+    @Body('id') id: string,
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description: string,
+  ): Item {
+    const item: Item = {
+      id: id,
+      name: name,
+      price: price,
+      description: description,
+      status: ItemStatus.ON_SALE,
+    };
+    return this.itemsService.create(item);
   }
 }
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Item } from './item.model';
 
 @Injectable()
 export class ItemsService {
+  // Item型の配列を作成
+  private items: Item[] = [];
+
   findAll() {
     return 'This is ItemsService';
+  }
+
+  // 配列にpush
+  create(item: Item): Item {
+    this.items.push(item);
+    return item;
   }
 }
